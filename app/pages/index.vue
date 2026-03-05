5bab9b0822e0da13c09ace9832825b11a31a5450
MrMiamm <cherwill@laposte.net>
2026-03-05 20:09:09 +0100

    Fix loading icon color

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,13 +4,13 @@
     <Title2 v-if="searchBar.status === 'error'" class="mb-4 text-red-500">Une erreur est survenue</Title2>
     <Title2 v-else-if="searchBar.status === 'success' && searchBar.jobs && searchBar.jobs.length === 0" class="mb-4">Aucune offre trouvée</Title2>
     <ContainersJobCards v-else-if="searchBar.status === 'success' && searchBar.jobs" :jobs="searchBar.jobs" />
-    <Icon v-else-if="searchBar.status === 'pending'" name="eos-icons:three-dots-loading" size="96" />
+    <Icon v-else-if="searchBar.status === 'pending'" class="text-primary" name="eos-icons:three-dots-loading" size="96" />
     
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { SearchResult } from '~/components/containers/SearchBar.vue';
+import type { SearchResult } from '~~/shared/types';
 
 const searchBar = ref<SearchResult>({
   status: 'pending',
