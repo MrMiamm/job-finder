0dae85c563e8b2244e73abd0f20a1c68aad62336
MrMiamm <cherwill@laposte.net>
2026-03-10 19:40:40 +0100

    Better pool configuration

--- server/utils/searchJobs.ts
+++ server/utils/searchJobs.ts
@@ -9,6 +9,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME,
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  keepAlive: true
 });
 
 interface TypeJobDB {
