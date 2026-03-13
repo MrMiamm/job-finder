14576c79c1c48dddca5ddff792e177aa9386650e
MrMiamm <cherwill@laposte.net>
2026-03-13 01:19:53 +0100

    W3C valid

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,10 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ]
+      ],
+      htmlAttrs: {
+        lang: 'fr',
+      },
     }
   },
   modules: [
