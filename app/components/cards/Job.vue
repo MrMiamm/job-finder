d3c27d126688926c9d8c832cbbdd019261b6f819
MrMiamm <cherwill@laposte.net>
2026-03-16 14:09:06 +0100

    Better job card responsive

--- app/components/cards/Job.vue
+++ app/components/cards/Job.vue
@@ -3,10 +3,10 @@
     :to="job.link"
     target="_blank"
     class="
-      group flex flex-row rounded-lg w-full
+      group flex flex-row rounded-lg w-full max-w-230 xl:max-w-full
       text-dark bg-secondary-bg/50 hover:bg-secondary-bg-hover
       border border-light-input hover:border-secondary-bg-hover
-      transition-all ease-in-out duration-300 cursor-pointer
+      transition-[background,border] ease-in-out duration-300 cursor-pointer
     "
   >
     <NuxtImg 
