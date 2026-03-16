4259157cb2b4d766ed946f38ab8578868b51509b
MrMiamm <cherwill@laposte.net>
2026-03-16 13:23:21 +0100

    Croix pour reset le texte d'un input

--- app/components/KeyIcon.vue
+++ app/components/KeyIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <Icon 
-    class="absolute -top-1 -right-1"
+    class="absolute -top-1 -right-1 z-5"
     :name="name" 
     :size="12" 
   />
--- app/components/inputs/Field.vue
+++ app/components/inputs/Field.vue
@@ -3,7 +3,7 @@
     class="
       relative flex flex-row gap-2 items-center
       text-dark-input bg-light-input shadow-sm
-      py-4 px-4 rounded-2xl cursor-text
+      py-4 pl-4 pr-12 rounded-2xl cursor-text
       border border-light
       transition-all ease-in-out duration-300
     "
@@ -24,6 +24,28 @@
     >
       <slot />
     </input>
+    <transition
+      enter-active-class="transition ease-out duration-150"
+      enter-from-class="opacity-0 scale-95 translate-x-2"
+      enter-to-class="opacity-100 scale-100 translate-x-0"
+      leave-active-class="transition ease-in duration-100"
+      leave-from-class="opacity-100 scale-100 translate-x-0"
+      leave-to-class="opacity-0 scale-95 translate-x-2"
+    >
+      <Button 
+        v-if="model"
+        class="
+          absolute right-0 top-0 bottom-0 px-3
+          flex items-center justify-center 
+          bg-light-input text-dark-input/80 
+          hover:text-dark-input
+          cursor-pointer rounded-2xl
+        "
+        @click="model = ''"
+      >
+        <Icon name="teenyicons:x-small-solid" size="24" />
+      </Button>
+    </transition>
   </span>
 </template>
 
